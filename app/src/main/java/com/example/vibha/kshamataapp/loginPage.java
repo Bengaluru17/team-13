76a7597217c3bd77b9301eceabf826534467b8b9
Larika Amlani <larika.amlani@gmail.com>
2017-07-08 22:47:54 +0530

    first commit

--- app/src/main/java/com/example/vibha/kshamataapp/loginPage.java
+++ app/src/main/java/com/example/vibha/kshamataapp/loginPage.java
@@ -1,13 +1,25 @@
 package com.example.vibha.kshamataapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 public class loginPage extends AppCompatActivity {
-
+EditText et1,et2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+et1= (EditText) findViewById(R.id.usernameText);
+        et2=(EditText)findViewById(R.id.passwordText);
+    }
+
+    public void login(View view) {
+String name=et1.getText().toString();
+        String pwd=et2.getText().toString();
+        String type="login";
+        Bw bw=new Bw(this);
+        bw.execute(type,name,pwd);
     }
 }
