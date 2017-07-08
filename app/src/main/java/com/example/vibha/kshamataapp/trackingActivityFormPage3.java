b65851dec243d8b40740885b7cc25f86d8d60a3f
Vibha Karanth <Vibha Karanth>
2017-07-08 19:09:14 +0530

    =page

--- app/src/main/java/com/example/vibha/kshamataapp/trackingActivityFormPage3.java
+++ app/src/main/java/com/example/vibha/kshamataapp/trackingActivityFormPage3.java
@@ -3,11 +3,11 @@ package com.example.vibha.kshamataapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class loginPage extends AppCompatActivity {
+public class trackingActivityFormPage3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_page);
+        setContentView(R.layout.activity_tracking_form_page3);
     }
 }
