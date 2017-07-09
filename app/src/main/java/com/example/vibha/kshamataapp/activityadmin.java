7c9678c4e57e3f33be655478bee9cac1fb8f047c
anushreeganta <anushree.ganta@gmail.com>
2017-07-09 01:46:41 -0700

    message3

---
+++ app/src/main/java/com/example/vibha/kshamataapp/activityadmin.java
@@ -0,0 +1,13 @@
+package com.example.vibha.kshamataapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activityadmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activityadmin);
+    }
+}
